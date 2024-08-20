22feeb2c4614545b71e9686bc71f2d2acbd0c4bb
Piyush-0369 <autri.piyush@gmail.com>
2024-08-20 16:39:39 +0530

    lab2

---
+++ Q7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    float x;
+    float y;
+    printf("Enter value of first number=");
+    scanf("%f",&x);
+    printf("Enter the value of second number=");
+    scanf("%f",&y);
+    float addition=x+y;
+    float subtraction=x-y;
+    float multiplication=x*y;
+    float division=x/y;
+    printf("For x=%f and y=%f\nThe sum =%f\nThe difference=%f\nThe product=%f\nThe quotient=%f",x,y,addition,subtraction,multiplication,division);
+    return 0;
+}
