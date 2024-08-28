9d9ad7b99d67adb196e79ba0fc29c676a8f5e443
Piyush-0369 <autri.piyush@gmail.com>
2024-08-28 09:41:49 +0530

    labwork

--- Q7.c
+++ Q7.c
@@ -1,25 +1,14 @@
 //LCM//
 #include <stdio.h>
-int prime(x){
-    for(int i=2; i<(x/2)+2; i++){
-        if(x%i==0){
-            break;
-        }
-        else{
-            return x;
-        }
-    }
-    return 0;
-}
 int main(){
-    int p,q,r,s,x,y;
-    printf("Enter first number");
-    scanf("%d",&x);
-    printf("Enter second number");
-    scanf("%d",&y);
-    int a=prime(x);
-    int b=prime(y);
-    if(a*b!=0){
-        printf("The LCM is %d",a*b);
+    int x,y,z,lcm;
+    printf("Enter two numbers you wanna check");
+    scanf("%d %d",&x,&y);
+    z=(x>y) ? x : y;
+    lcm=z;
+    while ((lcm%x!=0 || lcm%y!=0)){
+        lcm+=z;
     }
+    printf("The lcm is %d",lcm);
+    return 0;
 }
