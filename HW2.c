a175310605d8194bd6b4adf371fbf2aa65a71bc5
Piyush-0369 <autri.piyush@gmail.com>
2024-08-29 22:41:11 +0530

    homework

---
+++ HW2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    float P;
+    float r;
+    float T;
+    printf("Enter principal amount:");
+    scanf("%f",&P);
+    printf("Enter rate of intrest:");
+    scanf("%f",&r);
+    printf("Enter time(in years):");
+    scanf("%f",&T);
+    float R=r/100;
+    float SI=P*R*T;
+    printf("The simple intrest on the principal=%f on rate=%f for %f years is =%f",P,R,T,SI);
+    return 0;
+}
