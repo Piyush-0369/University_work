d6e3f87a47ab810f002bfb329f58ebe5324f6488
Piyush-0369 <autri.piyush@gmail.com>
2024-08-30 15:37:53 +0530

    homework

---
+++ HW3.c
@@ -0,0 +1,14 @@
+//Temp conversion//
+#include <stdio.h>
+int main(){
+    int choice;
+    float temp;
+    printf("What would you like to do\n1.Celsius to Farenheit\n2.Farenheit to Celsius\nEnter your choice(1 or 2):");
+    scanf("%d",&choice);
+    printf("Enter temprature:");
+    scanf("%f",&temp);     
+    float ntemp;
+    ntemp= choice==1 ? (temp*1.8)+32 : (temp-32)/(1.8);
+    printf("The converted temprature is=%f",ntemp);
+    return 0;
+}
