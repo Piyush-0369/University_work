36f7fd30d9b995f4b083b9a47cc0841f05bcc688
Piyush-0369 <autri.piyush@gmail.com>
2024-08-30 16:38:31 +0530

    labwork

--- Q3.c
+++ Q3.c
@@ -1,15 +1,21 @@
-//weighted sum//
+//area>perimere or not for rectangle//
 #include <stdio.h>
 int main(){
-    float m;
-    float p;
-    printf("Enter markrs in maths:");
-    scanf("%f",&m);
-    printf("Enter marks in physics:");
-    scanf("%f",&p);
-    float wm=(0.3)*m;
-    float wp=(0.7)*p;
-    float sum=wm+wp;
-    printf("Weighted sum=%f",sum);
+    float l,b;
+    printf("Enter length of rectangle:");
+    scanf("%f",&l);
+    printf("Enter breadth of rectangle:");
+    scanf("%f",&b);
+    float area=l*b;
+    float perimetre=2*(l+b);
+    if(area>perimetre){
+        printf("Area is greater than perimetre");
+    }
+    else if(area<perimetre){
+        printf("Area is less than perimetre");
+    }
+    else{
+        printf("Area and perimetre are equal");
+    }
     return 0;
 }
