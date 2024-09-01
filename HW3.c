9c2048effd0459ad1c2f06573f25c26d08a085df
Piyush-0369 <autri.piyush@gmail.com>
2024-09-01 21:25:31 +0530

    homework

--- HW3.c
+++ HW3.c
@@ -8,7 +8,16 @@ int main(){
     printf("Enter temprature:");
     scanf("%f",&temp);     
     float ntemp;
-    ntemp= choice==1 ? (temp*1.8)+32 : (temp-32)/(1.8);
-    printf("The converted temprature is=%f",ntemp);
+    while(choice!=1 || choice !=2){
+        printf("Enter valid choice 1 or 2");
+    }
+    if(choice==1){
+        ntemp=(temp*1.8)+32;
+        printf("The converted temprature is=%f",ntemp);
+    }
+    else{
+        ntemp=(temp-32)/(1.8);
+        printf("The converted temprature is=%f",ntemp);
+    }
     return 0;
 }
--- HW4.c
+++ HW4.c
@@ -1,1 +1,14 @@
 //odd even//
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter the number you want to check");
+    scanf("%d",&x);
+    if(x%2==0){
+        printf("The number is even");
+    }
+    else{
+        printf("The number is odd");
+    }
+    return 0;
+}
