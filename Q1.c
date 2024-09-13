e956bcfccb9680f3a3b81a6a14c266509f11a9d6
Piyush-0369 <autri.piyush@gmail.com>
2024-09-13 20:57:31 +0530

    labwork

---
+++ Q1.c
@@ -0,0 +1,12 @@
+//factorial//
+#include <stdio.h>
+int main(){
+    int x,f=1;
+    printf("Enter the number you want factorial for:");
+    scanf("%d",&x);
+    for(int i=1;i<(x+1);i++){
+        f=f*i;
+    }
+    printf("The factorial is %d",f);
+    return 0;
+}
