eb9e413c2cf166a851e07774fae887df7342fbed
Piyush Sharma <autri.piyush@gmail.com>
2024-10-22 21:30:15 +0530

    max_min_array

--- Q3.c
+++ Q3.c
@@ -2,7 +2,7 @@
 int MAX(int n,int A[n]){
     int max=A[0];
     for(int i=0;i<n;i++){
-        if(max>A[i]){
+        if(max<A[i]){
             max=A[i];
         }
     }
@@ -11,25 +11,21 @@ int MAX(int n,int A[n]){
 int MIN(int n,int A[n]){
     int min=A[0];
     for(int i=0;i<n;i++){
-        if(min<A[i]){
+        if(min>A[i]){
             min=A[i];
         }
     }
     return min;
-}
-void array(int n){
-    int A[n];
-    for(int i=0;i<n;i++){
-        printf("Enter the value of element number %d for the array:",i);
-        scanf("%d",&A[i]);
-    }
 }
 int main(){
     int n,max,min;
     printf("Enter size of array");
     scanf("%d",&n);
     int A[n];
-    array(n);
+    for(int i=0;i<n;i++){
+        printf("Enter the value of element number %d for the array:",i+1);
+        scanf("%d",&A[i]);
+    }
     max=MAX(n,A);
     min=MIN(n,A);
     printf("The maximum value in array is %d and minimum value is %d",max,min);
