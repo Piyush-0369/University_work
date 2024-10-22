38be41004d57a887737e5db39bf1dd94953355a2
Piyush Sharma <autri.piyush@gmail.com>
2024-10-22 21:38:54 +0530

    reverse_int

---
+++ Q4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int reverse(int x){
+    int y=0,z;
+    for(int i=0;x>0;i++){
+        z=x%10;
+        y=(y+z)*10;
+        x=x/10;
+    }
+    y=y/10;
+    return y;
+}
+int main(){
+    int x,y;
+    printf("Enter a number");
+    scanf("%d",&x);
+    y=reverse(x);
+    printf("The reverse of number %d is %d",x,y);
+    return 0;
+}
