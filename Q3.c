cc861acbd741eb6caa5b41dbe906a5fe4a7cb6a0
Piyush Sharma <autri.piyush@gmail.com>
2024-10-24 21:25:03 +0530

    reverse_string

--- Q3.c
+++ Q3.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 void reverse(int n,char A[n]){
-    char B[n];
-    int x=1;
+    char x;
     for(int i=0;i<n-1;i++){
-        B[i]=A[n-i-2];
+        x=A[i];
+        A[i]=A[n-i-2];
+        A[n-i-2]=x;
     }
 }
 int main(){
-    
+    int c,n=99999;
+    char A[n];
+    printf("Enter your word\n");
+    fgets(A,n,stdin);
+    n=strlen(A);
+    reverse(n,A);
+    printf("The reversed word is\n");
+    for(int i=0;i<n-1;i++){
+        printf("%c",A[i]);
+    }
 }
