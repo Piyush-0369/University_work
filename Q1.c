602bf5b5364ca15f973db6a007f89d934bac7246
Piyush Sharma <autri.piyush@gmail.com>
2024-11-05 19:29:42 +0530

    sum_x,y

--- Q1.c
+++ Q1.c
@@ -1,8 +1,8 @@
 //addition without user input//
 #include <stdio.h>
 int main(){
-    int x=1;
-    int y=2;
+    int x=5;
+    int y=10;
     int sum=x+y;
     printf("The sun of x and y is=%d",sum);
     return 0;
