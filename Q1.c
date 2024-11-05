415aa5aacf0e607b284d0987492855843cc17d5e
Piyush Sharma <autri.piyush@gmail.com>
2024-11-05 19:47:30 +0530

    voting_eligibility

--- Q1.c
+++ Q1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(){
     int age;
-    printf("Enter your age:");
+    printf("Enter your age: ");
     scanf("%d",&age);
     if(age>=18){
         printf("You are eligible to vote");
