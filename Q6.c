0ef71391710f221bf767d5e0cdb8b8ffc78964d0
Piyush Sharma <autri.piyush@gmail.com>
2024-11-05 19:44:44 +0530

    leap_year

--- Q6.c
+++ Q6.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(){
     int year;
-    printf("Enter a year");
+    printf("Enter the year you want to check");
     scanf("%d",&year);
     if(year%400==0){
         printf("%d is a leap year",year);
