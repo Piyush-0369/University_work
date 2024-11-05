003294483d2eb212b92d2b446b6e2f8dc2b754e2
Piyush Sharma <autri.piyush@gmail.com>
2024-11-05 19:37:58 +0530

    odd_or_even

--- HW4.c
+++ HW4.c
@@ -5,10 +5,10 @@ int main(){
     printf("Enter the number you want to check");
     scanf("%d",&x);
     if(x%2==0){
-        printf("The number is even");
+        printf("The given number is even");
     }
     else{
-        printf("The number is odd");
+        printf("The given number is odd");
     }
     return 0;
 }
