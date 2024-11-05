646b04d52cbe033f960e2cb4d2bb956841e7e60a
Piyush Sharma <autri.piyush@gmail.com>
2024-11-05 19:51:34 +0530

    area_vs_perimetre(rectangle)

--- Q3.c
+++ Q3.c
@@ -1,10 +1,10 @@
-//area>perimere or not for rectangle//
+//area>perimere or not. for rectangle//
 #include <stdio.h>
 int main(){
     float l,b;
-    printf("Enter length of rectangle:");
+    printf("Enter the length of rectangle:");
     scanf("%f",&l);
-    printf("Enter breadth of rectangle:");
+    printf("Enter the breadth of rectangle:");
     scanf("%f",&b);
     float area=l*b;
     float perimetre=2*(l+b);
