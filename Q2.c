7ccb27b5887263e81079d20717094f0d1f05a3f6
Piyush Sharma <autri.piyush@gmail.com>
2024-11-05 19:41:11 +0530

    sizeof(datatype)

--- Q2.c
+++ Q2.c
@@ -5,9 +5,9 @@ int main(){
     char q='c';
     float r=1.1;
     double s=2.2;
-    printf("The sizeof int is:%d\n",sizeof(p));
-    printf("The sizeof char is:%d\n",sizeof(q));
-    printf("The sizeof float is:%d\n",sizeof(r));
-    printf("The sizeof double is:%d\n",sizeof(s));
+    printf("The sizeof int is: %d\n",sizeof(p));
+    printf("The sizeof char is: %d\n",sizeof(q));
+    printf("The sizeof float is: %d\n",sizeof(r));
+    printf("The sizeof double is: %d\n",sizeof(s));
     return 0;
 }
