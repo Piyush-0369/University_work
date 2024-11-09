4dac2644374dd4a9c9f79024deef44885f039cab
Piyush Sharma <autri.piyush@gmail.com>
2024-11-09 18:23:15 +0530

    strcpy

---
+++ Q6.c
@@ -0,0 +1,15 @@
+//copying a string//
+#include<stdio.h>
+void main(){
+    int c,n=99999,i=0;
+    char A[n],B[n];
+    printf("Enter your first sentence\n");
+    fgets(A,n,stdin);
+    while(A[i]!='\0'){
+        B[i]=A[i];
+        i++;
+    }
+    B[i]='\0';
+    printf("String copied sucessfully\n");
+    printf("%s",B);
+}
