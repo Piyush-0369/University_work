6acca28cc4b12b05bb5ae75b1728e5c410157148
Piyush Sharma <autri.piyush@gmail.com>
2024-11-16 20:08:41 +0530

    not_working

--- Q7.c
+++ Q7.c
@@ -1,14 +1,44 @@
 #include<stdio.h>
 void main(){
-    int x=2;
-    FILE* file=fopen("test.txt","r");
-    if(file==NULL)
-        printf("File does not exist");
-    else{
-        char c;
-        while((c=fgetc(file))!=EOF){
-            printf("%c",c);
+    int c;
+        printf("\nWhich function would you like to use on the file\n1.Write\n2.Read\n3.Append\n4.Exit program\nEnter your choice:");
+        scanf("%d",&c);
+        if(c==1){
+            FILE* file;
+            file=fopen("sample.txt","w");
+            char str[99999];
+            printf("Enter what would you like to write in the file\n");
+            fgets(str,99999,file);
+            printf("Content added succesfully");
+            fclose(file);
         }
+        else if(c==2){
+            FILE* file;
+            file=fopen("sample.txt","r");
+            if(file==NULL)
+                printf("File does not exist");
+            else{
+                char c;
+                while((c=fgetc(file))!=EOF){
+                    printf("%c",c);
+                }
+            }
+            fclose(file);
+        }
+        else if(c==3){
+            FILE* file;
+            file=fopen("sample.txt","a");
+            if(file==NULL)
+                printf("File does not exist");
+            else{
+                char str[99999];
+                printf("Enter what would you like to write in the file\n");
+                fgets(str,99999,file);
+                printf("Content added succesfully");
+            }
+            fclose(file);
+        }
+        else{
+            printf("Invalid input try again");
     }
-    fclose(file);
 }
